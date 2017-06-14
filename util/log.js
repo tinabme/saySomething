fadb0ccc3d087fc19fad1e88fcb5344dbca90e32
tinabme <tinabme@users.noreply.github.com>
2017-06-14 05:24:01 -0400

    First pass at working saySomething app

---
+++ util/log.js
@@ -0,0 +1,13 @@
+const winston = require('winston');
+
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({ json: false, timestamp: true }),
+  ],
+  exceptionHandlers: [
+    new (winston.transports.Console)({ json: false, timestamp: true }),
+  ],
+  exitOnError: false
+});
+
+module.exports = logger;
